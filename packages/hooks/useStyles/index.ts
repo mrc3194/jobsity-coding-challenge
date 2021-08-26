ea81577ceef76b52ba4714c4295f6d34994b4478
Trener <trener@Manuels-MacBook-Pro.local>
2021-08-25 22:01:02 -0500

    Created useStyles hook

---
+++ packages/hooks/useStyles/index.ts
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Platform, StyleProp } from "react-native";
+import useDimensions from "../useDimensions";
+import useTheme from "../useTheme";
+import usePlatform from "../usePlatform";
+
+const useStyles = (stylesheet: Function, params: any): StyleProp<object> => {
+  const { screenWidth, screenHeight } = useDimensions();
+  const theme = useTheme();
+  const platform = usePlatform();
+  const dwPercentage = (number: number) => number * 0.01 * screenWidth;
+  const dhPercentage = (number: number) => number * 0.01 * screenHeight;
+  if (!stylesheet) {
+    return StyleSheet.create({});
+  }
+  const styles = stylesheet({
+    ...theme,
+    ...params,
+    screenHeight,
+    screenWidth,
+    ...platform,
+    dwPercentage,
+    dhPercentage,
+  });
+  return styles;
+};
+
+export default useStyles;
