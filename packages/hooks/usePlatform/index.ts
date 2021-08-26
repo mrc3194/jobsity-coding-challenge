deebb88bc8e4dfe424a25598d3603145ed3a48b9
Trener <trener@Manuels-MacBook-Pro.local>
2021-08-25 22:00:46 -0500

    Created usePlatform hook

---
+++ packages/hooks/usePlatform/index.ts
@@ -0,0 +1,18 @@
+import React from "react";
+import { Platform } from "react-native";
+
+const isAndroid = Platform.OS === "android";
+const isiOS = Platform.OS === "ios";
+
+interface usePlatformResponse {
+  isAndroid: boolean;
+  isiOS: boolean;
+  OS: string;
+  version: string | number;
+}
+
+const usePlatform = (): usePlatformResponse => {
+  return { isAndroid, isiOS, OS: Platform.OS, version: Platform.Version };
+};
+
+export default usePlatform;
