7cff7f2bea42e1dc2b2e3128828b0768e5fa215c
Trener <trener@Manuels-MacBook-Pro.local>
2021-08-26 22:54:58 -0500

    Created fetch/query to get series episodes

--- packages/common/queries/index.ts
+++ packages/common/queries/index.ts
@@ -36,6 +36,16 @@ const fetchSearch = async ({ queryKey }: any) => {
   }
 };
 
+const fetchEpisodes = async ({ queryKey }: any) => {
+  const { id } = queryKey[1];
+  try {
+    const response = await fetch(`https://api.tvmaze.com/shows/${id}/episodes`);
+    return response.json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 export const useFetchSeries = (page: number = 0) => {
   return useQuery(["allSeriesPaginated", { page }], fetchAllSeries);
 };
@@ -46,4 +56,8 @@ export const useSearchSeries = (searchTerm: string) => {
 
 export const useSeries = (id: number) => {
   return useQuery(["fetchSeries", { id }], fetchSeries);
+};
+
+export const useEpisodes = (id: number) => {
+  return useQuery(["fetchEpisodes", { id }], fetchEpisodes);
 };
