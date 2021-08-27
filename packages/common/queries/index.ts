1e97d41affe322a1ee2e62fd01733ffbf861d782
Trener <trener@Manuels-MacBook-Pro.local>
2021-08-26 22:23:39 -0500

    Created fetch/query to search series

--- packages/common/queries/index.ts
+++ packages/common/queries/index.ts
@@ -2,10 +2,11 @@ import React from "react";
 import { useQuery } from "react-query";
 import { SeriesObject } from "../types/queries/series";
 
-const fetchSeries = async (
-  page: number = 0
-): Promise<SeriesObject[] | void> => {
+const fetchAllSeries = async ({
+  queryKey,
+}: any): Promise<SeriesObject[] | void> => {
   try {
+    const { page } = queryKey[1];
     const response = await fetch(`https://api.tvmaze.com/shows?page=${page}`);
     return response.json();
   } catch (e) {
@@ -13,6 +14,22 @@ const fetchSeries = async (
   }
 };
 
+const fetchSearch = async ({ queryKey }: any) => {
+  const { searchTerm } = queryKey[1];
+  try {
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+    );
+    return response.json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 export const useFetchSeries = (page: number = 0) => {
-  return useQuery(["allSeriesPaginated", page], () => fetchSeries(page));
+  return useQuery(["allSeriesPaginated", { page }], fetchAllSeries);
+};
+
+export const useSearchSeries = (searchTerm: string) => {
+  return useQuery(["searchSeries", { searchTerm }], fetchSearch);
 };
