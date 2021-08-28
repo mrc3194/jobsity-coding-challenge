e40ea9c4aafe061b46b3cd23b0111048e2560b35
Trener <trener@Manuels-MacBook-Pro.local>
2021-08-28 00:43:32 -0500

    Added debounce function

---
+++ packages/common/debounce/index.ts
@@ -0,0 +1,17 @@
+function debounce(func: any, wait: number, immediate: boolean) {
+  var timeout: any;
+  return function (this: any) {
+    var context = this;
+    var args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+export default debounce;
